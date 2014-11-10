4f85f046bc32ac25b95451a4b7e5f360c002b12f
Luke Marsden <luke@hybridcluster.com>
2014-11-10 11:48:41 -0800

    Add alias to link

--- README.rst
+++ README.rst
@@ -12,11 +12,11 @@ Demo 1 - Docker
 Building and pushing a Docker image::
 
     docker-osx shell
-    docker build -t lmarsden/flask:v0.15 .; docker push lmarsden/flask:v0.15
+    docker build -t lmarsden/flask:v0.16 .; docker push lmarsden/flask:v0.16
 
 Running Docker::
 
-    docker run -p 80:80 lmarsden/flask:v0.15
+    docker run -p 80:80 lmarsden/flask:v0.16
 
 Check your web browser: http://localdocker/
 
--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from redis import Redis
 import os
 print os.environ, "<--"
 app = Flask(__name__)
-redis = Redis(host=os.environ.get("DOCKERFIGFLOCKERTUTORIAL_REDIS_1_PORT_6379_TCP_ADDR"),
-              port=os.environ.get("DOCKERFIGFLOCKERTUTORIAL_REDIS_1_PORT_6379_TCP_PORT"))
+redis = Redis(host=os.environ.get("REDIS_PORT_6379_TCP_ADDR"),
+              port=os.environ.get("REDIS_PORT_6379_TCP_PORT"))
 
 @app.route('/')
 def hello():
